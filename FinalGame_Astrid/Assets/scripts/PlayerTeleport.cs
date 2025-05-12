a23364000b27e59b0ab5b96ce1ebfb6e2b2de080
astrid <daphnecrespo9@gmail.com>
2025-05-12 00:19:55 -0400

    health bar added for player, light breaker added in second level

--- FinalGame_Astrid/Assets/scripts/PlayerTeleport.cs
+++ FinalGame_Astrid/Assets/scripts/PlayerTeleport.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIManager : MonoBehaviour
+public class PlayerTeleport : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,10 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            gameObject.transform.position = new Vector3(-19.1f, 0.6f, 20.9f);
         
+        }
     }
 }
